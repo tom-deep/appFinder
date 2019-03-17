74710f3a312f8eac7fcf6a2b87560baa616fe956
Michael Beck <beckmd@miamioh.edu>
2019-03-17 17:26:22 -0400

    Initial commit

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
+require('dotenv').config();
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const session = require('express-session');
+const FileStore = require('session-file-store')(session);
 
 const indexRouter = require('./routes/index');
 
@@ -17,6 +20,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  store: new FileStore(),
+  secret: 'keyboard cat',
+  resave: true,
+  saveUninitialized: true,
+}));
 
 app.use('/', indexRouter);
 
