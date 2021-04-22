451ad2e2112e2ddf973800023b6205f96817e3c6
Davis Heyman <heymand@miamioh.edu>
2021-04-22 14:51:04 -0400

    v07 - still need pictures

--- create.sql
+++ create.sql
@@ -1,3 +1,7 @@
+/*
+This file is not used, ended up making tables directly in the POSTGRESQL server. 
+*/
+
 CREATE TABLE users (
     id serial PRIMARY KEY,
     username character varying(255) UNIQUE,
